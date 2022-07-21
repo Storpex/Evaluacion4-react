0e356f684cff26055779cd6d2268b59d040fc165
Storpex <vex.videos@gmail.com>
2022-07-20 23:36:45 -0400

    readme modificado

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ En este caso usé 'yarn', y no 'npm'.
 # Para hacer andar el proyecto es:
 
 yarn dev
+
+# LINK YOUTUBE
+https://youtu.be/xIO9oS7IOiE
+
+# LINK PAGINA
+https://evaluacion4reactluis.netlify.app/
--- src/Intranet/Header.jsx
+++ src/Intranet/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
   return (
     <header className="header">
       <img className='header-logo' src={ logo } />
-      <h1 className="header-titulo">MONITORES</h1>
+      <h1 className="header-titulo">ARMAS TERRARIA</h1>
       <ul>
         <li>
           <NavLink to={'/intranet'} className='text-white' >Visualizar Todo</NavLink>
